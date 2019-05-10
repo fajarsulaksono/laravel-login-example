00e277f8905819c0029c32375cd3d84acaf8a74f
Fajar Sulaksono <fajar.sulaksono@gmail.com>
2019-05-10 13:46:12 +0700

    Move default route to middleware auth

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
